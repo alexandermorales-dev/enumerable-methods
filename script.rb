a53e59ad18159d733a42fe74c9e64f3d6aef6e9f
Alexander Morales <alexandermorales.n1@gmail.com>
2021-04-20 17:23:49 -0500

    add my_select method

--- script.rb
+++ script.rb
@@ -15,15 +15,27 @@ module Enumerable
       yield a, b
     end
   end
+
+  # my_select
+  def my_select
+    self.my_each do |num|
+      yield num
+    end
+  end
 end
 
 a = [1, 2, 3, 4, 5]
 
-# method calls
+# My method calls
+
 a.my_each do |num| # MY EACH
   num
 end
 
 a.my_each_with_index do |x, i| # MY EACH WITH INDEX
   p "the index of #{x} is #{i}"
+end
+
+a.my_select do |n| # MY SELECT
+  p n.even?
 end
