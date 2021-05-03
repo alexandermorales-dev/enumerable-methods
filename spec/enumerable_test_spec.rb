09954c51a59d23692b19f1f8714502aab4b9a901
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-02 21:58:49 -0500

    add test for my_each_with_index

--- spec/enumerable_test_spec.rb
+++ spec/enumerable_test_spec.rb
@@ -2,8 +2,16 @@ require './script.rb'
 
 describe Enumerable do
   describe "#my_each" do
-    it "returns each item in the iterable" do
-      expect([1, 2, 3].my_each { |x| x }).to eql([1, 2, 3])
+    it "prints each item in the iterable and returns original array" do
+      expect([1, 2, 3].my_each { |x| puts x }).to eql([1, 2, 3])
+    end
+  end
+end
+
+describe Enumerable do
+  describe "#my_each_with_index" do
+    it "prints current item and its index and returns original array" do
+      expect([4,5,6].my_each_with_index { |x,v| puts "#{x} is at position #{v}"}).to eql([4,5,6])
     end
   end
 end
